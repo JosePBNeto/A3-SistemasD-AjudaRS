ecf53f261b0cbc71adc6cb7c42ae934e6461e7f5
Jose Patricio Neto <josepatricioneto@hotmail.com>
2024-06-04 21:10:31 -0300

    first commit

---
+++ ajudaRS/src/main/java/app/ajudaRS/AjudaRsApplication.java
@@ -0,0 +1,13 @@
+package app.ajudaRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudaRsApplication.class, args);
+	}
+
+}
---
+++ ajudaRS/src/test/java/app/ajudaRS/AjudaRsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ajudaRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
